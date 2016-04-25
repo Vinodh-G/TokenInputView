7efc9bcd600bdae3b7b4c8ead9198f5cf45eb12b
Vinodh Swamy <vinodh.g.in@gmail.com>
2016-04-25 16:05:43 +0530

    Initial Commit

---
+++ TokenInputView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TokenInputView
+//
+//  Created by Vinodh Swamy on 4/25/16.
+//  Copyright © 2016 Vinodh Swamy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TokenInputView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TokenInputView
+//
+//  Created by Vinodh Swamy on 4/25/16.
+//  Copyright © 2016 Vinodh Swamy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
